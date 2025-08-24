57efb699bd8129b65edefd4b3664467d65b51ed4
Alles Abinash <allesabinash1@gmail.com>
2025-08-24 14:33:47 +0530

    final commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
+import React from "react";
 import "./App.css";
-
-import { Button } from "@/components/ui/button";
+import EcommerceStore from "./components/EcommerceStore";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="App">
+      <EcommerceStore />
     </div>
   );
 }
